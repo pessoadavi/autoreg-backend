6decb568600adf4386f0a31d611f7385386eafa8
Davi Pessoa <pessoadavi@gmail.com>
2020-06-25 00:48:10 -0300

    teste

--- src/main/java/com/project/autoreg/model/Regulador.java
+++ src/main/java/com/project/autoreg/model/Regulador.java
@@ -1,28 +1,21 @@
 package com.project.autoreg.model;
 
-import java.io.Serializable;
-import java.util.Date;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.validation.constraints.NotEmpty;
-
-import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonFormat.Shape;
-
-import org.springframework.format.annotation.DateTimeFormat;
+import java.io.Serializable;
 
 @Table
 @Entity /* gera uma tabela no DB a partir da classe */
 public class Regulador implements Serializable {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO) /* Gera o valor de id automaticamente*/
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO) /* Gera o valor de id automaticamente*/
     private Long id; /* id dado pela aplicação*/
 
     @NotEmpty
@@ -30,7 +23,7 @@ public class Regulador implements Serializable {
 
     @NotEmpty
     private String region; /* área onde se localiza o equipamento */
-    
+
     @NotEmpty
     private String feeder; /* alimentador */
 
@@ -74,93 +67,93 @@ public class Regulador implements Serializable {
 	//@DateTimeFormat(pattern = "dd/MM/yyyy")
 	private Date lastUpdate; /* data da última atualização */
 
-	public Long getId() {
-		return id;
-	}
+    public Long getId() {
+        return id;
+    }
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-	public String getCode() {
-		return code;
-	}
+    public String getCode() {
+        return code;
+    }
 
-	public void setCode(String code) {
-		this.code = code;
-	}
+    public void setCode(String code) {
+        this.code = code;
+    }
 
-	public String getRegion() {
-		return region;
-	}
+    public String getRegion() {
+        return region;
+    }
 
-	public void setRegion(String region) {
-		this.region = region;
-	}
+    public void setRegion(String region) {
+        this.region = region;
+    }
 
-	public String getFeeder() {
-		return feeder;
-	}
+    public String getFeeder() {
+        return feeder;
+    }
 
-	public void setFeeder(String feeder) {
-		this.feeder = feeder;
-	}
+    public void setFeeder(String feeder) {
+        this.feeder = feeder;
+    }
 
-	public String getBus() {
-		return bus;
-	}
+    public String getBus() {
+        return bus;
+    }
 
-	public void setBus(String bus) {
-		this.bus = bus;
-	}
+    public void setBus(String bus) {
+        this.bus = bus;
+    }
 
-	public String getModel() {
-		return model;
-	}
+    public String getModel() {
+        return model;
+    }
 
-	public void setModel(String model) {
-		this.model = model;
-	}
+    public void setModel(String model) {
+        this.model = model;
+    }
 
-	public String getVoltage() {
-		return voltage;
-	}
+    public String getVoltage() {
+        return voltage;
+    }
 
-	public void setVoltage(String voltage) {
-		this.voltage = voltage;
-	}
+    public void setVoltage(String voltage) {
+        this.voltage = voltage;
+    }
 
-	public String geteCurrent() {
-		return eCurrent;
-	}
+    public String geteCurrent() {
+        return eCurrent;
+    }
 
-	public void seteCurrent(String eCurrent) {
-		this.eCurrent = eCurrent;
-	}
+    public void seteCurrent(String eCurrent) {
+        this.eCurrent = eCurrent;
+    }
 
-	public String getlVoltage() {
-		return lVoltage;
-	}
+    public String getlVoltage() {
+        return lVoltage;
+    }
 
-	public void setlVoltage(String lVoltage) {
-		this.lVoltage = lVoltage;
-	}
+    public void setlVoltage(String lVoltage) {
+        this.lVoltage = lVoltage;
+    }
 
-	public String getManufacturer() {
-		return manufacturer;
-	}
+    public String getManufacturer() {
+        return manufacturer;
+    }
 
-	public void setManufacturer(String manufacturer) {
-		this.manufacturer = manufacturer;
-	}
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
 
-	public String getYearManufacture() {
-		return yearManufacture;
-	}
+    public String getYearManufacture() {
+        return yearManufacture;
+    }
 
-	public void setYearManufacture(String yearManufacture) {
-		this.yearManufacture = yearManufacture;
-	}
+    public void setYearManufacture(String yearManufacture) {
+        this.yearManufacture = yearManufacture;
+    }
 /*
 	public Date getDateEnergization() {
 		return dateEnergization;
@@ -186,7 +179,7 @@ public class Regulador implements Serializable {
 		this.lastUpdate = lastUpdate;
 	}
 
-*/    
+*/
 }
 
 
